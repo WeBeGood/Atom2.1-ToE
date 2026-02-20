061ea6476cea1b94e1b8f684bcfd427014972136
WeBeGood <34172802+WeBeGood@users.noreply.github.com>
2026-02-20 04:28:16 -0800

    paste_patch: show last commit hash after applying patch

--- paste_patch.bat
+++ paste_patch.bat
@@ -10,12 +10,21 @@ echo When finished, press Ctrl+Z then Enter.
 echo.
 echo (Tip: This window will stay open on errors.)
 echo.
+echo.
 
 python agent_apply.py --stdin
 
+set "COMMIT="
+for /f %%i in ('git rev-parse --short HEAD 2^>nul') do set "COMMIT=%%i"
+
 echo.
 echo ------------------------------------------
-echo Done (or exited). Press any key to close.
+if defined COMMIT (
+  echo Last commit: %COMMIT%
+) else (
+  echo Last commit: (unavailable)
+)
+echo Press any key to close.
 echo ------------------------------------------
 pause >nul
 endlocal
